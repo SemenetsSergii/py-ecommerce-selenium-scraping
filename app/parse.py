d3647ab9e6d773ee23949e26012d53518abe2337
Sergii Semenets <serhey.semenets@gmail.com>
2024-09-02 17:29:41 +0300

    fix according to the comments

--- app/parse.py
+++ app/parse.py
@@ -1,13 +1,14 @@
 import csv
+import os
 from dataclasses import dataclass, fields, astuple
-from typing import List
 from urllib.parse import urljoin
 
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
-
 
 BASE_URL = "https://webscraper.io/"
 HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
@@ -29,6 +30,7 @@ class Product:
     rating: int
     num_of_reviews: int
 
+
 PRODUCT_FIELDS = [field.name for field in fields(Product)]
 
 
@@ -45,14 +47,13 @@ def parse_single_product(product_soup: BeautifulSoup) -> Product:
     )
 
 
-def fetch_and_parse_products(
-        driver: WebDriver,
-        url: str,
-        with_pagination: bool
-) -> List[Product]:
-
+def fetch_and_parse_products(driver: WebDriver, url: str) -> list[Product]:
     driver.get(url)
-
+    with_pagination = bool(
+        driver.find_elements(By.CLASS_NAME,
+                             "ecomerce-items-scroll-more"
+                             )
+    )
     if with_pagination:
         while True:
             try:
@@ -61,45 +62,50 @@ def fetch_and_parse_products(
                     "ecomerce-items-scroll-more"
                 )
                 more_button.click()
-            except Exception as e:
-                print(e)
+            except Exception:
                 break
-
     page_soup = BeautifulSoup(
         driver.page_source,
-        "html.parser").select(".thumbnail")
-
+        "html.parser"
+    ).select(".thumbnail")
     return [parse_single_product(product_soup) for product_soup in page_soup]
 
 
-def write_to_csv(all_products: [Product], path_to_csv: str) -> None:
+def write_to_csv(all_products: list[Product], path_to_csv: str) -> None:
+    file_exists = os.path.isfile(f"{path_to_csv}.csv")
+    write_header = (not file_exists
+                    or os.path.getsize(f"{path_to_csv}.csv") == 0)
     with open(
-            f"{path_to_csv}.csv", "w",
+            f"{path_to_csv}.csv", "a" if file_exists else "w",
             newline="",
             encoding="utf-8"
     ) as csv_file:
 
         writer = csv.writer(csv_file)
-        writer.writerow(PRODUCT_FIELDS)
+        if write_header:
+            writer.writerow(PRODUCT_FIELDS)
+
         writer.writerows([astuple(product) for product in all_products])
 
 
-def process_urls(
-        driver: WebDriver,
-        urls: List[str],
-        with_pagination: bool
-) -> None:
+def process_urls(driver: WebDriver, urls: list[str]) -> None:
     for url in urls:
-        path_to_csv = url.split("/")[-1] if with_pagination \
-            else url.split("/")[-2].replace("more", "home")
-        products = fetch_and_parse_products(driver, url, with_pagination)
+        if url == HOME_URL:
+            path_to_csv = "home"
+        else:
+            path_to_csv = url.rstrip("/").split("/")[-1]
+
+        products = fetch_and_parse_products(driver, url)
         write_to_csv(products, path_to_csv)
 
 
 def get_all_products() -> None:
-    with webdriver.Firefox() as driver:
-        process_urls(driver, SIMPLE_URLS, with_pagination=False)
-        process_urls(driver, COMPLEX_URL, with_pagination=True)
+    option = webdriver.ChromeOptions()
+    option.add_argument("--headless")
+    service = Service(ChromeDriverManager().install())
+    with webdriver.Chrome(service=service, options=option) as driver:
+        process_urls(driver, SIMPLE_URLS)
+        process_urls(driver, COMPLEX_URL)
 
 
 if __name__ == "__main__":
